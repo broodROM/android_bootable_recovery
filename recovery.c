726794f94eb5fd686ca0d0ead22c250042a6d1c2
broodplank <guichardflorent@gmail.com>
2013-03-23 11:17:31 +0100

    Remove clockworkmod photobomb

--- recovery.c
+++ recovery.c
@@ -885,7 +885,6 @@ main(int argc, char **argv) {
         script_assert_enabled = 0;
         is_user_initiated_recovery = 1;
         ui_set_show_text(1);
-        ui_set_background(BACKGROUND_ICON_CLOCKWORK);
         
         if (extendedcommand_file_exists()) {
             LOGI("Running extendedcommand...\n");
